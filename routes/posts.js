a220a4d13999e4968b0c9a43a168de4db100fd6c
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-07-28 10:01:10 +0530

    update api

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { getPost, createPost } from '../controllers/posts.js';
+import { getPost, createPost, updatePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPost);
 router.post('/create', createPost);
+// router.get('/:id', getPost);
+router.put('/:id', updatePost);
+// router.delete('/:id', deletePost);
+// router.patch('/:id/likePost', likePost);
 
 export default router;
