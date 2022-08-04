0091b246cd5d1464b0c9e17561ce3dc1e05de5f4
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-04 18:27:38 +0530

    loading stage

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,10 +2,22 @@ import PostMessage from '../models/postModel.js';
 import mongoose from 'mongoose';
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
   try {
-    const postMessages = await PostMessage.find();
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const total = await PostMessage.countDocuments({});
 
-    res.status(200).json(postMessages);
+    const postMessages = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+
+    res.status(200).json({
+      data: postMessages,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
