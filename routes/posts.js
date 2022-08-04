1a746b1ae324c4bc1f4198024c47abe3b6b9548f
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-04 21:30:49 +0530

    detailspage

--- routes/posts.js
+++ routes/posts.js
@@ -6,15 +6,16 @@ import {
   updatePost,
   deletePost,
   likePost,
+  getPost,
 } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/:id', getPost);
 router.get('/search', getPostsBySearch);
 router.post('/create', auth, createPost);
-// router.get('/:id', getPost);
 router.put('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.put('/:id/likePost', auth, likePost);
